72d30de5a0c2d5bbf6117b32ef016f3d0736676b
MRangel7 <matheuscoutorangel7@gmail.com>
2023-10-22 15:25:49 -0300

    corrigindo os bugs do up passado (ainda falta resolver alguns)

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,363 +1,192 @@
-/* 1 modal */
-var modal = document.getElementById("modal");
-var btn = document.getElementById("ver-noticia-btn");
-var fecharBtn = document.getElementById("fechar-modal");
-
-btn.onclick = function () {
+// Função para abrir um modal
+function openModal(modal) {
     modal.style.display = "block";
 }
 
-fecharBtn.onclick = function () {
+// Função para fechar um modal
+function closeModal(modal) {
     modal.style.display = "none";
 }
 
-window.onclick = function (event) {
+// Função para tratar o evento de clique na janela
+function windowClick(event, modal) {
     if (event.target === modal) {
-        modal.style.display = "none";
+        closeModal(modal);
     }
 }
+/*7777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777*/
+var btn = document.getElementById("ver-noticia-btn");
+var modal = document.getElementById("modal");
+var fecharBtn = document.getElementById("fechar-modal");
 
-/* 2 modal */
-var modal2 = document.getElementById("modal-2");
+btn.onclick = function () {
+    openModal(modal);
+}
+
+fecharBtn.onclick = function () {
+    closeModal(modal);
+}
+
+window.onclick = function (event) {
+    windowClick(event, modal);
+}
+/*7777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777*/
 var btn2 = document.getElementById("ver-noticia-btn-2");
+var modal2 = document.getElementById("modal-2");
 var fecharBtn2 = document.getElementById("fechar-modal-2");
 
 btn2.onclick = function () {
-    modal2.style.display = "block";
+    openModal(modal2);
 }
 
 fecharBtn2.onclick = function () {
-    modal2.style.display = "none";
+    closeModal(modal2);
 }
 
 window.onclick = function (event) {
-    if (event.target === modal2) {
-        modal2.style.display = "none";
-    }
+    windowClick(event, modal2);
 }
-
-
-/* 3 modal */
-var modal3 = document.getElementById("modal-3");
+/*7777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777*/
 var btn3 = document.getElementById("ver-noticia-btn-3");
+var modal3 = document.getElementById("modal-3");
 var fecharBtn3 = document.getElementById("fechar-modal-3");
 
 btn3.onclick = function () {
-    modal3.style.display = "block";
+    openModal(modal3);
 }
 
 fecharBtn3.onclick = function () {
-    modal3.style.display = "none";
+    closeModal(modal3);
 }
 
 window.onclick = function (event) {
-    if (event.target === modal3) {
-        modal3.style.display = "none";
-    }
+    windowClick(event, modal3);
 }
-/* 4 modal */
-var modal4 = document.getElementById("modal-4");
+/*7777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777*/
 var btn4 = document.getElementById("ver-noticia-btn-4");
+var modal4 = document.getElementById("modal-4");
 var fecharBtn4 = document.getElementById("fechar-modal-4");
 
 btn4.onclick = function () {
-    modal4.style.display = "block";
+    openModal(modal4);
 }
 
 fecharBtn4.onclick = function () {
-    modal4.style.display = "none";
+    closeModal(modal4);
 }
 
 window.onclick = function (event) {
-    if (event.target === modal4) {
-        modal4.style.display = "none";
-    }
+    windowClick(event, modal4);
 }
-/* 5 modal */
-var modal5 = document.getElementById("modal-5");
+/*7777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777*/
 var btn5 = document.getElementById("ver-noticia-btn-5");
+var modal5 = document.getElementById("modal-5");
 var fecharBtn5 = document.getElementById("fechar-modal-5");
 
-fecharBtn5.onclick = function () {
-    modal5.style.display = "block";
+btn5.onclick = function () {
+    openModal(modal5);
 }
 
 fecharBtn5.onclick = function () {
-    modal5.style.display = "none";
+    closeModal(modal5);
 }
 
 window.onclick = function (event) {
-    if (event.target === modal5) {
-        modal5.style.display = "none";
-    }
+    windowClick(event, modal5);
 }
-/* 6 modal */
-var modal6 = document.getElementById("modal-6");
+/*7777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777*/
 var btn6 = document.getElementById("ver-noticia-btn-6");
+var modal6 = document.getElementById("modal-6");
 var fecharBtn6 = document.getElementById("fechar-modal-6");
 
 btn6.onclick = function () {
-    modal6.style.display = "block";
+    openModal(modal6);
 }
 
 fecharBtn6.onclick = function () {
-    modal6.style.display = "none";
+    closeModal(modal6);
 }
 
 window.onclick = function (event) {
-    if (event.target === modal6) {
-        modal6.style.display = "none";
-    }
+    windowClick(event, modal6);
 }
-/* 7 modal */
-var modal7 = document.getElementById("modal-7");
+/*7777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777*/
 var btn7 = document.getElementById("ver-noticia-btn-7");
+var modal7 = document.getElementById("modal-7");
 var fecharBtn7 = document.getElementById("fechar-modal-7");
 
 btn7.onclick = function () {
-    modal7.style.display = "block";
+    openModal(modal7);
 }
 
 fecharBtn7.onclick = function () {
-    modal7.style.display = "none";
+    closeModal(modal7);
 }
 
 window.onclick = function (event) {
-    if (event.target === modal7) {
-        modal7.style.display = "none";
-    }
+    windowClick(event, modal7);
 }
-/* 8 modal */
-var modal8 = document.getElementById("modal-8");
+/*7777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777*/
 var btn8 = document.getElementById("ver-noticia-btn-8");
+var modal8 = document.getElementById("modal-8");
 var fecharBtn8 = document.getElementById("fechar-modal-8");
 
 btn8.onclick = function () {
-    modal8.style.display = "block";
+    openModal(modal8);
 }
 
 fecharBtn8.onclick = function () {
-    modal8.style.display = "none";
+    closeModal(modal8);
 }
 
 window.onclick = function (event) {
-    if (event.target === modal8) {
-        modal8.style.display = "none";
-    }
+    windowClick(event, modal8);
 }
-/* 9 modal */
-var modal9 = document.getElementById("modal-9");
+/*7777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777*/
 var btn9 = document.getElementById("ver-noticia-btn-9");
+var modal9 = document.getElementById("modal-9");
 var fecharBtn9 = document.getElementById("fechar-modal-9");
 
 btn9.onclick = function () {
-    modal9.style.display = "block";
+    openModal(modal9);
 }
 
 fecharBtn9.onclick = function () {
-    modal9.style.display = "none";
+    closeModal(modal9);
 }
 
 window.onclick = function (event) {
-    if (event.target === modal9) {
-        modal9.style.display = "none";
-    }
+    windowClick(event, modal9);
 }
-/* 10 modal */
-var modal10 = document.getElementById("modal-10");
+/*7777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777*/
 var btn10 = document.getElementById("ver-noticia-btn-10");
-var fecharBtn10 = document.getElementById("fechar-modal-310");
+var modal10 = document.getElementById("modal-10");
+var fecharBtn10 = document.getElementById("fechar-modal-10");
 
 btn10.onclick = function () {
-    modal10.style.display = "block";
+    openModal(modal10);
 }
 
 fecharBtn10.onclick = function () {
-    modal10.style.display = "none";
+    closeModal(modal10);
 }
 
 window.onclick = function (event) {
-    if (event.target === modal10) {
-        modal10.style.display = "none";
-    }
+    windowClick(event, modal10);
 }
-/* 11 modal */
-var modal11 = document.getElementById("modal-11");
+/*7777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777*/
 var btn11 = document.getElementById("ver-noticia-btn-11");
+var modal11 = document.getElementById("modal-11");
 var fecharBtn11 = document.getElementById("fechar-modal-11");
 
 btn11.onclick = function () {
-    modal11.style.display = "block";
+    openModal(modal11);
 }
 
 fecharBtn11.onclick = function () {
-    modal11.style.display = "none";
+    closeModal(modal11);
 }
 
 window.onclick = function (event) {
-    if (event.target === modal11) {
-        modal11.style.display = "none";
-    }
-}
-/* 12 modal */
-var modal12 = document.getElementById("modal-12");
-var btn12 = document.getElementById("ver-noticia-btn-12");
-var fecharBtn12 = document.getElementById("fechar-modal-12");
-
-btn12.onclick = function () {
-    modal12.style.display = "block";
-}
-
-fecharBtn12.onclick = function () {
-    modal12.style.display = "none";
-}
-
-window.onclick = function (event) {
-    if (event.target === modal12) {
-        modal12.style.display = "none";
-    }
-}
-/* 13 modal */
-var modal13 = document.getElementById("modal-13");
-var btn13 = document.getElementById("ver-noticia-btn-13");
-var fecharBtn13 = document.getElementById("fechar-modal-13");
-
-btn13.onclick = function () {
-    modal13.style.display = "block";
-}
-
-fecharBtn13.onclick = function () {
-    modal13.style.display = "none";
-}
-
-window.onclick = function (event) {
-    if (event.target === modal13) {
-        modal13.style.display = "none";
-    }
-}
-/* 14 modal */
-var modal14 = document.getElementById("modal-14");
-var btn14 = document.getElementById("ver-noticia-btn-14");
-var fecharBtn14 = document.getElementById("fechar-modal-14");
-
-btn14.onclick = function () {
-    modal14.style.display = "block";
-}
-
-fecharBtn14.onclick = function () {
-    modal14.style.display = "none";
-}
-
-window.onclick = function (event) {
-    if (event.target === modal14) {
-        modal14.style.display = "none";
-    }
-}
-/* 15 modal */
-var modal15 = document.getElementById("modal-15");
-var btn15 = document.getElementById("ver-noticia-btn-15");
-var fecharBtn15 = document.getElementById("fechar-modal-15");
-
-btn15.onclick = function () {
-    modal15.style.display = "block";
-}
-
-fecharBtn15.onclick = function () {
-    modal15.style.display = "none";
-}
-
-window.onclick = function (event) {
-    if (event.target === modal15) {
-        modal15.style.display = "none";
-    }
-}
-/* 16 modal */
-var modal16 = document.getElementById("modal-16");
-var btn16 = document.getElementById("ver-noticia-btn-16");
-var fecharBtn16 = document.getElementById("fechar-modal-16");
-
-btn16.onclick = function () {
-    modal16.style.display = "block";
-}
-
-fecharBtn16.onclick = function () {
-    modal16.style.display = "none";
-}
-
-window.onclick = function (event) {
-    if (event.target === modal16) {
-        modal16.style.display = "none";
-    }
-}
-/* 17 modal */
-var modal17 = document.getElementById("modal-17");
-var btn17 = document.getElementById("ver-noticia-btn-17");
-var fecharBtn17 = document.getElementById("fechar-modal-17");
-
-btn17.onclick = function () {
-    modal17.style.display = "block";
-}
-
-fecharBtn17.onclick = function () {
-    modal17.style.display = "none";
-}
-
-window.onclick = function (event) {
-    if (event.target === modal17) {
-        modal17.style.display = "none";
-    }
-}
-/* 18 modal */
-var modal18 = document.getElementById("modal-18");
-var btn18 = document.getElementById("ver-noticia-btn-18");
-var fecharBtn18 = document.getElementById("fechar-modal-18");
-
-btn18.onclick = function () {
-    modal18.style.display = "block";
-}
-
-fecharBtn18.onclick = function () {
-    modal18.style.display = "none";
-}
-
-window.onclick = function (event) {
-    if (event.target === modal18) {
-        modal18.style.display = "none";
-    }
-}
-/* 19 modal */
-var modal19 = document.getElementById("modal-19");
-var btn19 = document.getElementById("ver-noticia-btn-19");
-var fecharBtn19 = document.getElementById("fechar-modal-19");
-
-btn19.onclick = function () {
-    modal19.style.display = "block";
-}
-
-fecharBtn19.onclick = function () {
-    modal19.style.display = "none";
-}
-
-window.onclick = function (event) {
-    if (event.target === modal19) {
-        modal19.style.display = "none";
-    }
-}
-/* 20 modal */
-var modal20 = document.getElementById("modal-20");
-var btn20 = document.getElementById("ver-noticia-btn-20");
-var fecharBtn20 = document.getElementById("fechar-modal-20");
-
-btn20.onclick = function () {
-    modal20.style.display = "block";
-}
-
-fecharBtn20.onclick = function () {
-    modal20.style.display = "none";
-}
-
-window.onclick = function (event) {
-    if (event.target === modal20) {
-        modal20.style.display = "none";
-    }
+    windowClick(event, modal11);
 }
