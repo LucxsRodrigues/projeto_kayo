a57f393ce0fc60ec28edcc7843a79d2e8b0b7f12
Matheus Benjamim <143435071+OmegaCampione@users.noreply.github.com>
2023-11-16 22:19:20 -0300

    Filtro
    rangel e burro

--- src/js/script.js
+++ src/js/script.js
@@ -37,3 +37,19 @@ function plusSlides(n) {
 
     slides[slideIndex - 1].style.display = 'block';
 }
+
+function filterProducts() {
+    var categoriaSelecionada = document.getElementById('filtroProdutos').value;
+    var produtos = document.querySelectorAll('.not1-container');
+  
+    produtos.forEach(function(produto) {
+        if (categoriaSelecionada === 'todos' || produto.classList.contains(categoriaSelecionada)) {
+            produto.style.display = 'block';
+        } else {
+            produto.style.display = 'none';
+        }
+    });
+  }
+  
+     // Chamar a função para exibir todos os produtos inicialmente
+     filterProducts();
